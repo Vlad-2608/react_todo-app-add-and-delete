11d29e3e7d6c5d050cc29afc7b28148ab3c90f1c
unknown <vladhafarov@gmail.com>
2025-06-13 18:39:08 +0300

    solution

---
+++ src/components/ClearCompletedButton/ClearCompletedButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface ClearCompletedButtonProps {
+  todoCompleted: boolean;
+  handleClearCompletedButton: () => void;
+}
+
+export const ClearCompletedButton: React.FC<ClearCompletedButtonProps> = ({
+  todoCompleted,
+  handleClearCompletedButton,
+}) => {
+  return (
+    /* this button should be disabled if there are no completed todos */
+    <button
+      type="button"
+      className="todoapp__clear-completed"
+      data-cy="ClearCompletedButton"
+      disabled={!todoCompleted}
+      onClick={handleClearCompletedButton}
+    >
+      Clear completed
+    </button>
+  );
+};
